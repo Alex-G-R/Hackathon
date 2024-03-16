f466a4112450be6ecc83556a1bb8295aeeb4d126
Alex-G-R <alex.grabski555@gmail.com>
2024-03-16 06:14:15 +0100

    buba

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
-from chat_bot import *
+from flaskcors import CORS
+from chatbot import *
 
-app = Flask(__name__)
+app = Flask(name)
 CORS(app)
 
 
 @app.route('/test', methods=['POST'])
 def test():
-    data = request.json
-    if 'name' in data and len(data['name']) > 10:
-        return jsonify({"Result": data['name']})
-    else:
-        return jsonify({"Error": "Name should be provided and longer than 10 characters."}), 400
+    with ChatBotManager(ChatBot("Bot", "John")) as bot:
+        data = request.json
+        if 'name' in data and len(data['name']) > 10:
+            result = bot.offensivewordsin(data["name"])
+            return jsonify({"Result": result})
+        else:
+            return jsonify({"Error": "Name should be provided and longer than 10 characters."}), 400
 
 
-if __name__ == '__main__':
+if __name == '__main':
     app.run(debug=True)
